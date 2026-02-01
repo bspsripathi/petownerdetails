8033c495980399a00620a5a939c584632c49c9cf
sripathi <sripathi.bachu@gmail.com>
2026-02-01 22:38:09 +0530

    "Application setup"

---
+++ src/main/java/com/petdetails/petdetails/PetdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.petdetails.petdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetdetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petdetails/petdetails/PetdetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petdetails.petdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetdetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
